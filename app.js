fc3fddf748404bd88f66ca50385c6386e1f23510
rania sekhara <115374869+Ran-sekhara@users.noreply.github.com>
2024-03-08 17:13:11 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const apiRoutes = require('./routes/api');
 const sequelize = require('./config/database');
-require('dotenv').config()
 
 const app = express();
-app.use(bodyParser.json());
+app.use(cors()); // Enable CORS
 app.use(express.json());
 
 sequelize.authenticate()
